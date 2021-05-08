81ddb6ce41eb9e3d7bd520080719de2cf4b1908e
ebrooker <18298060+ebrooker@users.noreply.github.com>
2021-05-08 15:00:02 -0400

    [Test] Created proctorClass derived type to handle reusable test formats. Will expand further as time goes by and more numerical methods are added to package.

---
+++ src/Test/proctorClass.f90
@@ -0,0 +1,188 @@
+!!
+!!
+!!
+MODULE proctorClass
+USE kindSettings, ONLY : rkp
+USE functionInterface, ONLY : f_1d_intrfc
+USE numericalDifferentiation
+IMPLICIT NONE
+PRIVATE
+
+    PUBLIC :: proctor_1d
+
+    CHARACTER(LEN=50) :: line="!!************************************************"
+    CHARACTER(LEN=2 ) :: excl="!!"
+
+    TYPE proctor_1d
+
+        PROCEDURE(f_1d_intrfc), POINTER, NOPASS :: f
+        PROCEDURE(f_1d_intrfc), POINTER, NOPASS :: df
+        REAL(rkp)                               :: x = 0.0
+        REAL(rkp)                               :: h = 0.1
+        REAL(rkp)                               :: y = 0.0
+        REAL(rkp)                               :: deriv(3,3) = 0.0
+        REAL(rkp)                               :: error(3,3) = 0.0
+        INTEGER                                 :: ideriv
+        CHARACTER(LEN=3)                        :: cderiv
+
+        CONTAINS
+            PROCEDURE :: init => constructor
+            PROCEDURE :: dtest
+            PROCEDURE :: compute
+            PROCEDURE :: output
+            PROCEDURE :: rextest => richExtrapTest
+            PROCEDURE :: set_f
+            FINAL     :: destructor
+
+    END TYPE proctor_1d
+
+CONTAINS
+
+    SUBROUTINE destructor(this)
+        TYPE(proctor_1d) :: this
+
+        ! WRITE(*,*) "[PROCTOR_1D] Deallocating dynamic arrays..."
+        ! IF (ALLOCATED(this%x    )) DEALLOCATE(this%x    )
+        ! IF (ALLOCATED(this%h    )) DEALLOCATE(this%h    )
+        ! IF (ALLOCATED(this%deriv)) DEALLOCATE(this%deriv)
+        ! IF (ALLOCATED(this%error)) DEALLOCATE(this%error)
+
+        WRITE(*,*) "[PROCTOR_1D] Nullifying procedure pointers..."
+        IF (ASSOCIATED(this%f )) this%f  => NULL()
+        IF (ASSOCIATED(this%df)) this%df => NULL()
+
+        WRITE(*,*) "[PROCTOR_1D] Class instance destroyed..."
+    END SUBROUTINE destructor
+
+    SUBROUTINE constructor(this,fin,dfin,ideriv,xin,hin)
+        CLASS(proctor_1d)               :: this
+        PROCEDURE(f_1d_intrfc)          :: fin
+        PROCEDURE(f_1d_intrfc)          :: dfin
+        REAL(rkp), OPTIONAL, INTENT(IN) :: xin
+        REAL(rkp), OPTIONAL, INTENT(IN) :: hin
+        INTEGER,             INTENT(IN) :: ideriv
+
+        CALL this%set_f(fin,dfin,ideriv)
+        IF (PRESENT(xin)) this%x = xin
+        IF (PRESENT(hin)) this%h = hin
+
+    END SUBROUTINE constructor
+
+
+    SUBROUTINE set_f(this,fin,dfin,ideriv)
+        CLASS(proctor_1d)      :: this
+        PROCEDURE(f_1d_intrfc) :: fin
+        PROCEDURE(f_1d_intrfc) :: dfin
+        INTEGER, INTENT(IN)    :: ideriv
+
+        IF (ASSOCIATED(this%f )) this%f  => NULL()
+        this%f  => fin
+        IF (ASSOCIATED(this%df)) this%df => NULL()
+        this%df => dfin
+
+        SELECT CASE (ideriv)
+            CASE (1)
+                this%cderiv="1st"
+            CASE (2)
+                this%cderiv="2nd"
+            CASE (3)
+                this%cderiv="3rd"
+            CASE(4)
+                this%cderiv="4th"
+            CASE default
+                this%cderiv="1st"
+        END SELECT
+        this%ideriv = ideriv
+
+    END SUBROUTINE set_f
+
+
+    SUBROUTINE dtest(this)
+        CLASS(proctor_1d) :: this
+        CALL this%compute()
+        CALL this%output()
+        CALL this%rextest()
+
+    END SUBROUTINE dtest
+
+
+    SUBROUTINE compute(this)
+        CLASS(proctor_1d) :: this
+        this%deriv(1,1) = cntrDiff(this%f,this%x,this%h,deriv=this%ideriv        )
+        this%deriv(1,2) = cntrDiff(this%f,this%x,this%h,deriv=this%ideriv,order=2)
+        this%deriv(1,3) = cntrDiff(this%f,this%x,this%h,deriv=this%ideriv,order=4)
+        this%deriv(2,1) = fwrdDiff(this%f,this%x,this%h,deriv=this%ideriv        )
+        this%deriv(2,2) = fwrdDiff(this%f,this%x,this%h,deriv=this%ideriv,order=1)
+        this%deriv(2,3) = fwrdDiff(this%f,this%x,this%h,deriv=this%ideriv,order=2)
+        this%deriv(3,1) = bwrdDiff(this%f,this%x,this%h,deriv=this%ideriv        )
+        this%deriv(3,2) = bwrdDiff(this%f,this%x,this%h,deriv=this%ideriv,order=1)
+        this%deriv(3,3) = bwrdDiff(this%f,this%x,this%h,deriv=this%ideriv,order=2)
+    END SUBROUTINE compute
+
+
+    SUBROUTINE output(this)
+        CLASS(proctor_1d) :: this
+        write(*,*) ""
+        write(*,*) line
+        write(*,*) excl
+        write(*,*) "!! Computing ",this%cderiv," derivative"
+        write(*,*) excl
+        write(*,*) "!! cntrDiff default =", this%deriv(1,1)
+        write(*,*) "!! cntrDiff 2nd ord =", this%deriv(1,2)
+        write(*,*) "!! cntrDiff 4th ord =", this%deriv(1,3)
+        write(*,*) "!! ",this%cderiv," derivative   =", this%df(this%x)
+        write(*,*) excl
+        write(*,*) "!! fwrdDiff default =", this%deriv(2,1)
+        write(*,*) "!! fwrdDiff 1st ord =", this%deriv(2,2)
+        write(*,*) "!! fwrdDiff 2nd ord =", this%deriv(2,3)
+        write(*,*) "!! ",this%cderiv," derivative   =", this%df(this%x)
+        write(*,*) excl
+        write(*,*) "!! bwrdDiff default =", this%deriv(3,1)
+        write(*,*) "!! bwrdDiff 1st ord =", this%deriv(3,2)
+        write(*,*) "!! bwrdDiff 2nd ord =", this%deriv(3,3)
+        write(*,*) "!! ",this%cderiv," derivative   =", this%df(this%x)
+        write(*,*) excl
+        write(*,*) line
+        write(*,*) ""
+
+    END SUBROUTINE output
+
+
+    SUBROUTINE richExtrapTest(this)
+        CLASS(proctor_1d) :: this
+        REAL(rkp)         :: yh(3),yh2(3)
+
+        this%deriv(1,1) = cntrDiff(this%f,this%x,0.5*this%h,deriv=this%ideriv,order=2)
+        this%deriv(2,1) = fwrdDiff(this%f,this%x,0.5*this%h,deriv=this%ideriv,order=1)        
+        this%deriv(3,1) = bwrdDiff(this%f,this%x,0.5*this%h,deriv=this%ideriv,order=1)        
+
+        write(*,*) ""
+        write(*,*) line
+        write(*,*) excl
+        write(*,*) "!! Computing ",this%cderiv," derivative"
+        write(*,*) excl
+        write(*,*) "!! cntrDiff 2nd ord h/2 =", this%deriv(1,1)
+        write(*,*) "!! cntrDiff 2nd ord h   =", this%deriv(1,2)
+        write(*,*) "!! cntrDiff 4th ord h   =", this%deriv(1,3)
+        write(*,*) "!! cntrDiff richExtrap  =", richExtrap(this%deriv(1,2),this%deriv(1,1))
+        write(*,*) "!! ",this%cderiv," derivative       =", this%df(this%x)
+        write(*,*) excl
+        write(*,*) "!! fwrdDiff 1st ord h/2 =", this%deriv(2,1)
+        write(*,*) "!! fwrdDiff 1st ord h   =", this%deriv(2,2)
+        write(*,*) "!! fwrdDiff 2nd ord h   =", this%deriv(2,3)
+        write(*,*) "!! fwrdDiff richExtrap  =", richExtrap(this%deriv(2,2),this%deriv(2,1))
+        write(*,*) "!! ",this%cderiv," derivative       =", this%df(this%x)
+        write(*,*) excl
+        write(*,*) "!! bwrdDiff 1st ord h/2 =", this%deriv(3,1)
+        write(*,*) "!! bwrdDiff 1st ord h   =", this%deriv(3,2)
+        write(*,*) "!! bwrdDiff 2nd ord h   =", this%deriv(3,3)
+        write(*,*) "!! bwrdDiff richExtrap  =", richExtrap(this%deriv(3,2),this%deriv(3,1))
+        write(*,*) "!! ",this%cderiv," derivative       =", this%df(this%x)
+        write(*,*) excl
+        write(*,*) line
+        write(*,*) ""
+
+    END SUBROUTINE richExtrapTest
+
+
+END MODULE proctorClass
--- src/Test/test.f90
+++ src/Test/test.f90
@@ -1,191 +1,23 @@
 PROGRAM test
-USE kindSettings, ONLY : rkp, ilkp, iskp
-USE numericalDifferentiation
+USE kindSettings, ONLY : rkp
 USE funcs
 USE derivatives
+USE proctorClass, ONLY : proctor_1d
 IMPLICIT NONE
-
-	REAL(rkp) :: x, h, ext(2), y1(3), y2(3)
-
-	h = 0.01
-	x = 2.0
-	ext = 1.0
-
-    write(*,*) ""
-    write(*,*) "!!************************************************"
-    write(*,*) "!!"
-    write(*,*) "!! 1st derivative test #1"
-    write(*,*) "!!"
-    write(*,*) "!! cntrDiff default =", cntrDiff_d1(f1,x,h        )
-    write(*,*) "!! cntrDiff 2nd ord =", cntrDiff_d1(f1,x,h,order=2)
-    write(*,*) "!! cntrDiff 4th ord =", cntrDiff_d1(f1,x,h,order=4)
-    write(*,*) "!! 1st derivative   =", df1(x)
-    write(*,*) "!!"
-    write(*,*) "!! fwrdDiff default =", fwrdDiff_d1(f1,x,h        )
-    write(*,*) "!! fwrdDiff 1st ord =", fwrdDiff_d1(f1,x,h,order=1)
-    write(*,*) "!! fwrdDiff 2nd ord =", fwrdDiff_d1(f1,x,h,order=2)
-    write(*,*) "!! 1st derivative   =", df1(x)
-    write(*,*) "!!"
-    write(*,*) "!! bwrdDiff default =", bwrdDiff_d1(f1,x,h        )
-    write(*,*) "!! bwrdDiff 1st ord =", bwrdDiff_d1(f1,x,h,order=1)
-    write(*,*) "!! bwrdDiff 2nd ord =", bwrdDiff_d1(f1,x,h,order=2)
-    write(*,*) "!! 1st derivative   =", df1(x)
-    write(*,*) "!!"
-    write(*,*) "!!************************************************"
-    write(*,*) ""
-
-    write(*,*) ""
-    write(*,*) "!!************************************************"
-    write(*,*) "!!"
-    write(*,*) "!! 2nd derivative test #1"
-    write(*,*) "!!"
-    write(*,*) "!! cntrDiff default =", cntrDiff_d2(f1,x,h        )
-    write(*,*) "!! cntrDiff 2nd ord =", cntrDiff_d2(f1,x,h,order=2)
-    write(*,*) "!! cntrDiff 4th ord =", cntrDiff_d2(f1,x,h,order=4)
-    write(*,*) "!! 2nd derivative   =", d2f1(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!! fwrdDiff default =", fwrdDiff_d2(f1,x,h        )
-    write(*,*) "!! fwrdDiff 1st ord =", fwrdDiff_d2(f1,x,h,order=1)
-    write(*,*) "!! fwrdDiff 2nd ord =", fwrdDiff_d2(f1,x,h,order=2)
-    write(*,*) "!! 2nd derivative   =", d2f1(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!! bwrdDiff default =", bwrdDiff_d2(f1,x,h        )
-    write(*,*) "!! bwrdDiff 1st ord =", bwrdDiff_d2(f1,x,h,order=1)
-    write(*,*) "!! bwrdDiff 2nd ord =", bwrdDiff_d2(f1,x,h,order=2)
-    write(*,*) "!! 2nd derivative   =", d2f1(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!!************************************************"
-    write(*,*) ""
-
-    write(*,*) ""
-    write(*,*) "!!************************************************"
-    write(*,*) "!!"
-    write(*,*) "!! 1st derivative test #2"
-    write(*,*) "!!"
-    write(*,*) "!! cntrDiff default =", cntrDiff_d1(f2,x,h,        args=ext)
-    write(*,*) "!! cntrDiff 2nd ord =", cntrDiff_d1(f2,x,h,order=2,args=ext)
-    write(*,*) "!! cntrDiff 4th ord =", cntrDiff_d1(f2,x,h,order=4,args=ext)
-    write(*,*) "!! 1st derivative   =", df2(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!! fwrdDiff default =", fwrdDiff_d1(f2,x,h,        args=ext)
-    write(*,*) "!! fwrdDiff 1st ord =", fwrdDiff_d1(f2,x,h,order=1,args=ext)
-    write(*,*) "!! fwrdDiff 2nd ord =", fwrdDiff_d1(f2,x,h,order=2,args=ext)
-    write(*,*) "!! 1st derivative   =", df2(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!! bwrdDiff default =", bwrdDiff_d1(f2,x,h,        args=ext)
-    write(*,*) "!! bwrdDiff 1st ord =", bwrdDiff_d1(f2,x,h,order=1,args=ext)
-    write(*,*) "!! bwrdDiff 2nd ord =", bwrdDiff_d1(f2,x,h,order=2,args=ext)
-    write(*,*) "!! 1st derivative   =", df2(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!!************************************************"
-    write(*,*) ""
-
-
-    write(*,*) ""
-    write(*,*) "!!************************************************"
-    write(*,*) "!!"
-    write(*,*) "!! 2nd derivative test #2"
-    write(*,*) "!!"
-    write(*,*) "!! cntrDiff default =", cntrDiff_d2(f2,x,h,        args=ext)
-    write(*,*) "!! cntrDiff 2nd ord =", cntrDiff_d2(f2,x,h,order=2,args=ext)
-    write(*,*) "!! cntrDiff 4th ord =", cntrDiff_d2(f2,x,h,order=4,args=ext)
-    write(*,*) "!! 2nd derivative   =", d2f2(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!! fwrdDiff default =", fwrdDiff_d2(f2,x,h,        args=ext)
-    write(*,*) "!! fwrdDiff 1st ord =", fwrdDiff_d2(f2,x,h,order=1,args=ext)
-    write(*,*) "!! fwrdDiff 2nd ord =", fwrdDiff_d2(f2,x,h,order=2,args=ext)
-    write(*,*) "!! 2nd derivative   =", d2f2(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!! bwrdDiff default =", bwrdDiff_d2(f2,x,h,        args=ext)
-    write(*,*) "!! bwrdDiff 1st ord =", bwrdDiff_d2(f2,x,h,order=1,args=ext)
-    write(*,*) "!! bwrdDiff 2nd ord =", bwrdDiff_d2(f2,x,h,order=2,args=ext)
-    write(*,*) "!! 2nd derivative   =", d2f2(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!!************************************************"
-    write(*,*) ""
-
-
-    write(*,*) ""
-    write(*,*) "!!************************************************"
-    write(*,*) "!!"
-    write(*,*) "!! 3rd derivative test"
-    write(*,*) "!!"
-    write(*,*) "!! cntrDiff default =", cntrDiff_d3(f2,x,h,        args=ext)
-    write(*,*) "!! cntrDiff 2nd ord =", cntrDiff_d3(f2,x,h,order=2,args=ext)
-    write(*,*) "!! cntrDiff 4th ord =", cntrDiff_d3(f2,x,h,order=4,args=ext)
-    write(*,*) "!! 3rd derivative   =", d3f2(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!! fwrdDiff default =", fwrdDiff_d3(f2,x,h,        args=ext)
-    write(*,*) "!! fwrdDiff 1st ord =", fwrdDiff_d3(f2,x,h,order=1,args=ext)
-    write(*,*) "!! fwrdDiff 2nd ord =", fwrdDiff_d3(f2,x,h,order=2,args=ext)
-    write(*,*) "!! 3rd derivative   =", d3f2(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!! bwrdDiff default =", bwrdDiff_d3(f2,x,h,        args=ext)
-    write(*,*) "!! bwrdDiff 1st ord =", bwrdDiff_d3(f2,x,h,order=1,args=ext)
-    write(*,*) "!! bwrdDiff 2nd ord =", bwrdDiff_d3(f2,x,h,order=2,args=ext)
-    write(*,*) "!! 3rd derivative   =", d3f2(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!!************************************************"
-    write(*,*) ""
-
-
-    write(*,*) ""
-    write(*,*) "!!************************************************"
-    write(*,*) "!!"
-    write(*,*) "!! 4th derivative test"
-    write(*,*) "!!"
-    write(*,*) "!! cntrDiff default =", cntrDiff_d4(f2,x,h,        args=ext)
-    write(*,*) "!! cntrDiff 2nd ord =", cntrDiff_d4(f2,x,h,order=2,args=ext)
-    write(*,*) "!! cntrDiff 4th ord =", cntrDiff_d4(f2,x,h,order=4,args=ext)
-    write(*,*) "!! 4th derivative   =", d4f2(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!! fwrdDiff default =", fwrdDiff_d4(f2,x,h,        args=ext)
-    write(*,*) "!! fwrdDiff 1st ord =", fwrdDiff_d4(f2,x,h,order=1,args=ext)
-    write(*,*) "!! fwrdDiff 2nd ord =", fwrdDiff_d4(f2,x,h,order=2,args=ext)
-    write(*,*) "!! 4th derivative   =", d4f2(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!! bwrdDiff default =", bwrdDiff_d4(f2,x,h,        args=ext)
-    write(*,*) "!! bwrdDiff 1st ord =", bwrdDiff_d4(f2,x,h,order=1,args=ext)
-    write(*,*) "!! bwrdDiff 2nd ord =", bwrdDiff_d4(f2,x,h,order=2,args=ext)
-    write(*,*) "!! 4th derivative   =", d4f2(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!!************************************************"
-    write(*,*) ""
-
-    y1(1) = cntrDiff_d4(f2,x,    h,order=2,args=ext)
-    y2(1) = cntrDiff_d4(f2,x,0.5*h,order=2,args=ext)
-
-    y1(2) = fwrdDiff_d4(f2,x,    h,order=1,args=ext)
-    y2(2) = fwrdDiff_d4(f2,x,0.5*h,order=1,args=ext)
     
-    y1(3) = bwrdDiff_d4(f2,x,    h,order=1,args=ext)
-    y2(3) = bwrdDiff_d4(f2,x,0.5*h,order=1,args=ext)
+    TYPE(proctor_1d) :: proctor
+    REAL(rkp) :: x=2.0, h=0.01
 
-    write(*,*) ""
-    write(*,*) "!!************************************************"
-    write(*,*) "!!"
-    write(*,*) "!! 4th derivative test w/ Richardson Extrapolation"
-    write(*,*) "!!"
-    write(*,*) "!! cntrDiff 2nd ord    =", y1(1)
-    write(*,*) "!! cntrDiff 2nd ord    =", y2(1)
-    write(*,*) "!! cntrDiff 4th ord    =", cntrDiff_d4(f2,x,    h,order=4,args=ext)
-    write(*,*) "!! cntrDiff richExtrap =", richExtrap(y1(1),y2(1))
-    write(*,*) "!! 4th derivative      =", d4f2(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!! fwrdDiff 1st ord    =", y1(2) 
-    write(*,*) "!! fwrdDiff 1st ord    =", y2(2) 
-    write(*,*) "!! fwrdDiff 2nd ord    =", fwrdDiff_d4(f2,x,    h,order=2,args=ext)
-    write(*,*) "!! fwrdDiff richExtrap =", richExtrap(y1(2),y2(2))
-    write(*,*) "!! 4th derivative      =", d4f2(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!! bwrdDiff 1st ord    =", y1(3)
-    write(*,*) "!! bwrdDiff 1st ord    =", y2(3)
-    write(*,*) "!! bwrdDiff 2nd ord    =", bwrdDiff_d4(f2,x,    h,order=2,args=ext)
-    write(*,*) "!! bwrdDiff richExtrap =", richExtrap(y1(3),y2(3))
-    write(*,*) "!! 4th derivative      =", d4f2(x,args=ext)
-    write(*,*) "!!"
-    write(*,*) "!!************************************************"
-    write(*,*) ""
+    CALL proctor%init(fin=f2,dfin=df2,ideriv=1,xin=x,hin=h)
+    CALL proctor%dtest()
 
+    CALL proctor%set_f(fin=f2,dfin=d2f2,ideriv=2)
+    CALL proctor%dtest()
+
+    CALL proctor%set_f(fin=f2,dfin=d3f2,ideriv=3)
+    CALL proctor%dtest()
+
+    CALL proctor%set_f(fin=f2,dfin=d4f2,ideriv=4)
+    CALL proctor%dtest()
 
 END PROGRAM test
